ce19d35b456e04454f14e1f78ca3d29487506194
Ilia Znamenskii <ilia@appsmith.com>
2024-07-25 15:04:52 +0200

    ignore eval

--- app/client/src/workers/Evaluation/indirectEval.ts
+++ app/client/src/workers/Evaluation/indirectEval.ts
@@ -1,5 +1,8 @@
 export default function indirectEval(script: string) {
-  /* Indirect eval to prevent local scope access. 
-  Ref. - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/eval#description */
+  // Do not remove ts-ignore!
+  // We want evaluation to be done only on global scope and shouldn’t have access to any local scope variable.
+  // Ref. - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/eval#description */
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
   return (1, eval)(script);
 }
